be1b2da15f587aa9bd810c9e93767fe06bc4eaae
DELL <moreti68@gmail.com>
2025-06-25 12:27:26 -0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Passou')
+        print('Passou nesse teste')
     else:
-        print('Falhou')
+        print('Falhou nesse teste')
 
 in_autotests_we_trust(10, '10')
 
